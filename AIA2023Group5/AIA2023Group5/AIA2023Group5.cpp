43fc54be819f1de78ffb020da90d968550eac682
LAPTOP-SJITUAD1\Larry Luo <larry.luo@acer.com>
2023-05-06 01:55:33 +0800

    check concentrated

--- AIA2023Group5/AIA2023Group5/AIA2023Group5.cpp
+++ AIA2023Group5/AIA2023Group5/AIA2023Group5.cpp
@@ -59,9 +59,12 @@ cv::Size frameSize = stringToSize(sizeString);
 cv::Size downSize = cv::Size(640 / 3, 360 / 3);
 cv::Size downSizeVideo = cv::Size(1280 - (640 / 3) - 10, 720 - (360 / 3));
 cv::Size reSize = cv::Size(640*1.5 , 360*1.5);
+
+cv::Mat status = cv::Mat(cv::Size(1000, 100), CV_8UC3);
+
 std::unique_ptr<ImagesCapture> cap;
 
-std::string FLAGS_m_fd = "..\\intel\\face-detection-retail-0004\\FP16\\face-detection-retail-0004.xml"
+std::string FLAGS_m_fd = "..\\intel\\face-detection-retail-0004\\FP32\\face-detection-retail-0004.xml"
 , FLAGS_d_fd = "GPU"
 , FLAGS_m_hp = "..\\intel\\head-pose-estimation-adas-0001\\FP32\\head-pose-estimation-adas-0001.xml"
 , FLAGS_d_hp = "GPU"
@@ -75,6 +78,10 @@ std::string FLAGS_m_fd = "..\\intel\\face-detection-retail-0004\\FP16\\face-dete
 ResultsMarker resultsMarker(true, true, true, true, true);
 
 FaceDetector *faceDetector;
+HeadPoseEstimator *headPoseEstimator;
+LandmarksEstimator *landmarksEstimator;
+EyeStateEstimator *eyeStateEstimator;
+GazeEstimator *gazeEstimator;
 // Put pointers to all estimators in an array so that they could be processed uniformly in a loop
 std::vector< BaseEstimator*> estimators;
 
@@ -150,7 +157,7 @@ void loadDB() {
     }
 }
 
-//#define DO_ESTIMATORS
+#define DO_ESTIMATORS
 void Init() {
 
     // Load OpenVINO runtime
@@ -165,16 +172,28 @@ void Init() {
     faceRecognizer = FaceRecognizerSF::create(FLAGS_m_fr, "");
 
 #ifdef DO_ESTIMATORS
-    HeadPoseEstimator headPoseEstimator(core, FLAGS_m_hp, FLAGS_d_hp);
-    LandmarksEstimator landmarksEstimator(core, FLAGS_m_lm, FLAGS_d_lm);
-    EyeStateEstimator eyeStateEstimator(core, FLAGS_m_es, FLAGS_d_es);
-    GazeEstimator gazeEstimator(core, FLAGS_m, FLAGS_d);
-    estimators.push_back(&headPoseEstimator);
-    estimators.push_back(&landmarksEstimator);
-    estimators.push_back(&eyeStateEstimator);
-    estimators.push_back(&gazeEstimator);
+    headPoseEstimator=new HeadPoseEstimator(core, FLAGS_m_hp, FLAGS_d_hp);
+    landmarksEstimator=new LandmarksEstimator (core, FLAGS_m_lm, FLAGS_d_lm);
+    eyeStateEstimator=new EyeStateEstimator (core, FLAGS_m_es, FLAGS_d_es);
+    gazeEstimator=new GazeEstimator (core, FLAGS_m, FLAGS_d);
+    estimators.push_back(headPoseEstimator);
+    estimators.push_back(landmarksEstimator);
+    estimators.push_back(eyeStateEstimator);
+    estimators.push_back(gazeEstimator);
 #endif // DO_ESTIMATORS
 
+}
+
+void Release() {
+
+    if (faceDetector)delete faceDetector;
+
+    if (headPoseEstimator)delete headPoseEstimator;
+    if (landmarksEstimator)delete  landmarksEstimator;
+    if (eyeStateEstimator)delete eyeStateEstimator;
+    if (gazeEstimator)delete gazeEstimator;
+
+    estimators.clear();
 }
 
 int sceneStatus = 0;
@@ -182,7 +201,7 @@ int sceneStatus = 0;
 cv::Mat RunScene1(cv::Mat canvas) {
 
     cv::Mat frame = cap->read();
-    resize(frame, frame, reSize, INTER_LINEAR);
+    cv::resize(frame, frame, reSize, INTER_LINEAR);
 
     auto inferenceResults = faceDetector->detect(frame);
 
@@ -255,13 +274,13 @@ cv::Mat RunScene1(cv::Mat canvas) {
     if (cvui::button(canvas, reSize.width+x,y, "LOGIN")) {
 
         if (isMember) {
-            canvas.setTo(cv::Scalar(0, 0, 0));
-            cv::putText(canvas, "Welcome!! " + names[maxFace]+" please wait.....", cv::Point(10, 50), cv::FONT_HERSHEY_SIMPLEX, 2, cv::Scalar(0, 255, 0), 2); // 在圖像上添加警報文字
+            status.setTo(cv::Scalar(0, 0, 0));
+            cv::putText(status, "Welcome!! " + names[maxFace]+" please wait.....", cv::Point(10, 50), cv::FONT_HERSHEY_SIMPLEX, 2, cv::Scalar(0, 255, 0), 2); // 在圖像上添加警報文字
             sceneStatus = 2;
         }
         else {
-            canvas.setTo(cv::Scalar(0, 0, 0));
-            cv::putText(canvas, "Login Fail" , cv::Point(10, 50), cv::FONT_HERSHEY_SIMPLEX, 2, cv::Scalar(0, 0, 255), 2); // 在圖像上添加警報文字
+            status.setTo(cv::Scalar(0, 0, 0));
+            cv::putText(status, "Login Fail" , cv::Point(10, 50), cv::FONT_HERSHEY_SIMPLEX, 2, cv::Scalar(0, 0, 255), 2); // 在圖像上添加警報文字
         }
         
     }
@@ -270,39 +289,63 @@ cv::Mat RunScene1(cv::Mat canvas) {
 
 }
 
+std::mutex mu;
+std::vector<float> recordStatus;
+
 cv::Mat RunScene2(cv::Mat canvas) {
 
     cv::Mat frame = cap->read();
     cv::Size graphSize{ frame.cols / 4, 60 };
-
     // Infer results
     if (!estimators.empty()) {
+        //find main face
+        int maxArea = 0;
+        int maxFace = -1;
         auto inferenceResults = faceDetector->detect(frame);
-        for (auto& inferenceResult : inferenceResults) {
+        for (int i = 0; i < inferenceResults.size(); i++) {
+
+            auto& inferenceResult = inferenceResults[i];
             for (auto estimator : estimators) {
                 estimator->estimate(frame, inferenceResult);
+            }
+
+            int area = inferenceResult.faceBoundingBox.width * inferenceResult.faceBoundingBox.height;
+            if (area > maxArea)
+            {
+                maxArea = area;
+                maxFace = i;
             }
         }
 
         float gazeH = 100;
         float gazeV = 100;
-        if (inferenceResults.empty())
-        {
-        }
-        else
-        {
-            auto const& inferenceResult = inferenceResults[0];
+        if (maxFace >= 0){
+            auto const& inferenceResult = inferenceResults[maxFace];
             resultsMarker.mark(frame, inferenceResult);
             cv::Point2f gazeAngles;
             gazeVectorToGazeAngles(inferenceResult.gazeVector, gazeAngles);
             gazeH = gazeAngles.x;
             gazeV = gazeAngles.y;
-
         }
+
         cv::putText(frame, "gaze angle H: " + std::to_string(std::round(gazeH)) + " V: " + std::to_string(std::round(gazeV)), cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 0, 255), 2);
 
+        //check gaze
+        {
+            std::unique_lock<std::mutex> locker(mu);
+            if (fabs(gazeH) > 20 || fabs(gazeV) > 12) {
+                recordStatus.push_back(1);
+            }
+            else  {
+                recordStatus.push_back(0);
+            }
+            locker.unlock();
+        }
+
     }
-    resize(frame, frame, downSize, INTER_LINEAR);
+
+
+    cv::resize(frame, frame, downSize, INTER_LINEAR);
 
     int x = canvas.cols - downSize.width - 10;
     int y = 10;
@@ -337,32 +380,7 @@ int main()
     // Create a frame
     cv::Mat canvas = cv::Mat(cv::Size(horizontal, vertical), CV_8UC3);
 
-    // cv::Mat cameraFrame = cv::Mat(downSize, CV_8UC3);
-
     isRunning = true;
-    /*
-    *  VideoCapture vid_capture("https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4");
-    // Print error message if the stream is invalid
-    if (!vid_capture.isOpened())
-    {
-        std::cout << "Error opening video stream or file\n";
-    }
-     std::thread([&]() {
-        while (isRunning)
-        {
-            Mat frame;
-            // Initialize a boolean to check if frames are there or not
-            bool isSuccess = vid_capture.read(frame);
-
-            // If frames are present, show it
-            if (isSuccess == true)
-            {
-                resize(frame, frame, downSizeVideo, INTER_LINEAR);
-                cvui::image(canvas, 0, 0, frame);
-            }
-        }
-        }).detach();
-    */
    
     while (isRunning)
     {
@@ -386,9 +404,10 @@ int main()
             }
             else
             {
+                status.setTo(cv::Scalar(0, 0, 0));
                 canvas.setTo(cv::Scalar(0, 0, 0));
                 std::thread([&](VideoCapture vid_capture) {
-                        while (sceneStatus >= 2)
+                        while (sceneStatus >= 2 && isRunning)
                         {
                             Mat frame;
                             // Initialize a boolean to check if frames are there or not
@@ -413,7 +432,30 @@ int main()
 
                         canvas.setTo(cv::Scalar(0, 0, 0));
                     }, vid_capture).detach();
-                    sceneStatus = 3;
+                sceneStatus = 3;
+                std::thread([&]() {
+                    while (sceneStatus == 3 && isRunning)
+                    {
+                        std::unique_lock<std::mutex> locker(mu);
+                        if (!recordStatus.empty()) {
+
+                            std::cout << "recordStatus.size():" << recordStatus.size() << "\n";
+                            float avg = std::accumulate(recordStatus.begin(), recordStatus.end(), 0.0f) / recordStatus.size();
+                            std::cout << "avg:"<< avg<<"\n";
+                            if (avg > 0.4) {
+                                cv::putText(status, "Not concentrated", cv::Point(10, 50), cv::FONT_HERSHEY_SIMPLEX, 2, cv::Scalar(0, 0, 255), 2); // 在圖像上添加警報文字
+                            }
+                            else {
+                                status.setTo(cv::Scalar(0, 0, 0));
+                            }
+
+                        }
+
+                        recordStatus.clear();
+                        locker.unlock();
+                        Sleep(1500);
+                    }
+                    }).detach();
             }
         }
         else if (sceneStatus == 3) {
@@ -421,12 +463,15 @@ int main()
         }
 
         cvui::update();
+        cvui::image(canvas, 0,0, status);
         cvui::imshow(windowName,canvas);
         if (waitKey(1) == 27) {
             isRunning = false;
             Sleep(1000);
             break; }
     }
+
+    Release();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
